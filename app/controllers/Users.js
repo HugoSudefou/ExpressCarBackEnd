562418abc42a7300a7f4a95382ef1f41e433daa5
HugoSudefou <hugo.sudefou@gmail.com>
2016-03-11 16:01:13 +0100

    update

--- app/controllers/Users.js
+++ app/controllers/Users.js
@@ -1,136 +1,83 @@
 require('../models/User');
-var https = require('https');
 var mongoose = require('mongoose'),
-    User = mongoose.model('User');
+User = mongoose.model('User');
 
-function isEmpty(value) {
-    return value == undefined || value == "";
-
+function isEmpty(value){
+	return value == undefined || value == "";
+	
 }
 
-function isEmptyChamp(req) {
-    var regexEmail = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.([a-z]+)|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
-
-    return isEmpty(req.body.username) || isEmpty(req.body.name) || isEmpty(req.body.firstname) || !regexEmail.test(req.body.email) || isEmpty(req.body.password) ||
-        isEmpty(req.body.passwordV) || isEmpty(req.body.phonenumber) || isEmpty(req.body.address)  || isEmpty(req.body.postalcode) || isEmpty(req.body.city)
-}
-
-function verifyIfPhoneAndFirstNameAreNotUndefined(req){
-    if(req.body.phonenumber == undefined){
-        req.body.phonenumber = "";
-    }
-    if(req.body.firstname == undefined){
-        req.body.firstname = "";
-    }
+function isEmptyChamp(req){
+	var regexEmail = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.([a-z]+)|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
+	
+	return isEmpty(req.body.username) || isEmpty(req.body.name) || isEmpty(req.body.firstname) || !regexEmail.test(req.body.email) || isEmpty(req.body.password) || isEmpty(req.body.passwordV) || isEmpty(req.body.phonenumber) || isEmpty(req.body.address)
 }
 
 //Creer un utilisateur
-var error = [];
 var Users = {
-    create: function (req, res) {
+	create : function(req, res) {
 
-        User.findOne({'email': req.body.email}, function (err, userInBase) {
-            if (userInBase) {
-                error.push("l'adresse email est déja utilisé");
-            }
+		console.log("sfdq");
+		return 0;
 
-            if (isEmptyChamp(req)) {
-                error.push("un champ est incorrect ou manquant");
-            }
+		var error = [];
+		User.findOne({'email' : req.body.email}, function(err, userInBase){
+			if(userInBase){
+				error.push("l'adresse email est déja utilisé");
+			}
+			
+			if(isEmptyChamp(req)){
+				error.push("un champ est incorrect ou manquant");
+			}
+			
+			if(req.body.password != req.body.passwordV){
+				error.push("les mots de passe ne corresepondent pas");
+			}
+			
+			if(error == ""){
+				var newUser = new User({
+					username : req.body.username,
+					name : req.body.name,
+					firstname : req.body.firstname,
+					email : req.body.email,
+					password : req.body.password,
+					passwordV : req.body.passwordV,
+					phonenumber : req.body.phonenumber,
+					address : req.body.address,
 
-            if (req.body.password != req.body.passwordV) {
-                error.push("les mots de passe ne corresepondent pas");
-            }
-
-            if(verifyIfPhoneAndFirstNameAreNotUndefined(req)){
-                error.push("Certaines valeurs sont incorrect");
-            }
-
-            var datasMaps = "";
-            var addressInLine = req.body.address + ", "+ req.body.postalcode + ", " + req.body.city + ", " + req.body.country;
-            //var addressInLine = req.body.address;
-            var options = {
-                host: "maps.googleapis.com",
-                path: '/maps/api/geocode/json?address=' + addressInLine.replace(/\s/g, "+") + '&key=AIzaSyBh-ZMhtx_g97Xs2ZLBryqd8ldApqo_veI'
-            };
-
-            https.get(options, function(res){
-                res.setEncoding('utf8');
-
-                res.on('data',function(chunk){
-                    datasMaps += chunk;
-                });
-                res.on('end', function(){
-                   datasMaps = JSON.parse(datasMaps);
-                   if(datasMaps.status != "OK"){
-                       error.push("L'adresse est introuvable");
-                   }
-                    WriteInbase(req);
-                });
-
-                function WriteInbase(req) {
-                    if (error.length == 0) {
-                        var addressComponents = datasMaps.results[0].address_components;
-                        var coordinates = datasMaps.results[0].geometry.location;
-                        //apeller la fonction qui va trouver la latitude et longitude en fonction de l'adresse
-                        var newUser = new User({
-                            username: req.body.username,
-                            name: req.body.name,
-                            firstname: req.body.firstname,
-                            email: req.body.email,
-                            password: req.body.password,
-                            passwordV: req.body.passwordV,
-                            phonenumber: req.body.phonenumber,
-                            address: addressComponents[0].long_name + " " + addressComponents[1].long_name,
-                            postalCode: parseInt(addressComponents[6].long_name, 10),
-                            city: addressComponents[2].long_name,
-                            country: addressComponents[5].long_name,
-                            latitude: coordinates.lat,
-                            longitude: coordinates.lng
-                        });
-
-                        newUser.save(function (err) {
-                            if (err) {
-                                throw err;
-                            }
-                            console.log("L'Utilisateur a été crée!!!!!!!!");
-                            console.log(u);
-                        });
-                        res.render("index", {title: "Carea"});//a modifier
-                    }
-                }
-            });
-
-            res.render("signup", {title: "CaRea", form: req.body, error: error});
-        });
-
-
-
-
-    },
-
-    signIn: function (req, res) {
-        User.findOne({'email': req.body.email}, function (err, user) {
-            if (user) {
-                user.comparePassword(req.body.password, function (err, isMatch) {
-                    if (isMatch) {
-                        req.session.isAuthentificated = true;
-                        // permet de pouvoir via un findOne de recuperer les données de lutilisateur dans la base
-                        req.session.email = user.email;
-                        //req.redirect(req.url + ""); ligne a modifier
-                    } else {
-                        // A modifier
-                        error.push("Le mot de passe est incorrect");
-                        req.render("signIn", {title:"Carea", error: error});
-                    }
-                });
-            }else{
-                error.push("Le mot de passe est incorrect");
-                req.render("signIn",{title: "Carea", error: error});
-            }
-        });
-    }
-};
-
+				});
+			
+				newUser.save(function(err){
+					if(err){
+						throw err;
+					}
+					console.log("L'Utilisateur a été crée!!!!!!!!");
+					console.log(u);
+				});
+				res.render("index", {title: "Carea"});
+			}
+			
+			res.render("signup", {title: "Carea", form: req.body, error: error});
+		});
+	},
+	
+	signIn : function(req,  res){
+		User.findOne({'email': req.body.email}, function(err, user){
+			if(user){
+				user.comparePassword(req.body.password, function (err, isMatch) {
+					 if (isMatch) {
+							 req.session.isAuthentificated = true;
+							 req.session.email = user.email; // permet de pouvoir via un findOne de recuperer les données de lu'tilisateur dans la base
+							 
+							 //Mettre la redirection 
+					} else {
+							// A modifier
+							req.redirect(req.url+"");
+					}
+				});
+			}
+		});
+	}
+};	
 
 module.exports = Users;
--- app/routes/users.js
+++ app/routes/users.js
@@ -7,4 +7,8 @@ router.post('/signup', Users.create);
 router.get('/signup', function(req,res){
 	res.render('signup');
 })
+router.post('/signin', Users.signIn);
+router.get('/signin', function(req,res){
+	res.render('signin');
+})
 module.exports = router;
