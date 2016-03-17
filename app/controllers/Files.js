a63e873fcfa4d25632e072109557f8bdf2afe6f6
HugoSudefou <hugo.sudefou@gmail.com>
2016-03-17 11:58:22 +0100

    jade

--- app/controllers/Files.js
+++ app/controllers/Files.js
@@ -4,19 +4,19 @@
 
 var Files = {
     index: function(req, res){
-        res.render('index', { title: 'Express' });
+        res.render('index', { title: 'Express', session: req.session.isAuthentificated});
     },
 
     about: function(req,res){
-        res.render('about');
+        res.render('about', {form:{}, session: req.session.isAuthentificated});
     },
 
     signIn: function(req,res){
-        res.render('signin');
+        res.render('signin', {form:{}, session: req.session.isAuthentificated});
     },
 
     signUp: function(req,res){
-        res.render('signup', {form:{}});
+        res.render('signup', {form:{}, session: req.session.isAuthentificated});
     },
     construct: function(req,res){
         res.render('construct');
